128c25dfb80ef35de408cef64180951a1aba46b3
HugoVasc <hugo.silva08@hotmail.com>
2022-07-05 21:46:38 -0300

    criacao do objeto e geracao de numeros aleatorios

---
+++ SM/client.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+class cliente:
+  clientId: 0b0000000000
+  produtor: False;
+  consumo: 0b0000000000
+  valorCompra: 0b0000000000
+  producao: 0b0000000000
+  valorVenda: 0b0000000000
+
+casa = cliente()
+
+casa.clientId = bin(randint(0,1024))
+casa.produtor = bool(randint(0,1))
+casa.consumo = bin(randint(0,1024))
+casa.valorCompra = bin(randint(0,1024))
+casa.producao = bin(randint(0,1024))
+casa.valorVenda = bin(randint(0,1024))
+
+print('O cliente ' + casa.clientId + ' consumiu ' + casa.consumo + ' e vai pagar ' + casa.valorCompra + ' por esse consumo.')
+print('O cliente ' + str(int(casa.clientId, 2)) + ' consumiu ' + str(int(casa.consumo, 2)) + ' e vai pagar ' + str(int(casa.valorCompra, 2)) + ' por esse consumo.')
