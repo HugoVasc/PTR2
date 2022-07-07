38b2154cfce4fea1f2ef867b7030700a49efe347
HugoVasc <hugo.silva08@hotmail.com>
2022-07-06 21:37:39 -0300

    corrigindo o range de geração de numeros aleatorios para caber dentro de 10 bits

--- SM/client.py
+++ SM/client.py
@@ -1,6 +1,7 @@
 from random import randint
 
 
+
 class cliente:
   clientId: 0b0000000000
   produtor: False;
@@ -11,12 +12,12 @@ class cliente:
 
 casa = cliente()
 
-casa.clientId = bin(randint(0,1024))
+casa.clientId = bin(randint(0,1023))
 casa.produtor = bool(randint(0,1))
-casa.consumo = bin(randint(0,1024))
-casa.valorCompra = bin(randint(0,1024))
-casa.producao = bin(randint(0,1024))
-casa.valorVenda = bin(randint(0,1024))
+casa.consumo = bin(randint(0,1023))
+casa.valorCompra = bin(randint(0,1023))
+casa.producao = bin(randint(0,1023))
+casa.valorVenda = bin(randint(0,1023))
 
 print('O cliente ' + casa.clientId + ' consumiu ' + casa.consumo + ' e vai pagar ' + casa.valorCompra + ' por esse consumo.')
 print('O cliente ' + str(int(casa.clientId, 2)) + ' consumiu ' + str(int(casa.consumo, 2)) + ' e vai pagar ' + str(int(casa.valorCompra, 2)) + ' por esse consumo.')
