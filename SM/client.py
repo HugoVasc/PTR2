576bcff729e18908b126a7adf998d2855bc6cad3
Hugo Vasconcelos <hugo.silva08@hotmail.com>
2022-07-13 22:27:28 -0300

    adicionando a funcao lfsr

--- SM/client.py
+++ SM/client.py
@@ -4,12 +4,24 @@ from numpy import matrix
 
 
 class client:
-  clientId: 0b0000000000
+  clientId: 0b0
   produtor: False;
-  consumo: 0b0000000000
-  valorCompra: 0b0000000000
-  producao: 0b0000000000
-  valorVenda: 0b0000000000
+  consumo: 0b0
+  valorCompra: 0b0
+  producao: 0b0
+  valorVenda: 0b0
+
+def lfsr(seed, mask):
+    result = seed
+    nbits = mask.bit_length()-1
+    while True:
+        result = (result << 1)
+        xor = result >> nbits
+        if xor != 0:
+            result ^= mask
+
+        yield xor, result
+      
 
 def getRandomValues():
   cliente = client()
@@ -57,13 +69,13 @@ def createMatrix(line:bitarray):
   
 
 casa = getRandomValues()
-line = getRandomPositions(casa)
+#line = getRandomPositions(casa)
 
 print('O cliente ' + casa.clientId + ' consumiu ' + casa.consumo + ' e vai pagar ' + casa.valorCompra + ' por esse consumo.')
 print('O cliente ' + str(int(casa.clientId, 2)) + ' consumiu ' + str(int(casa.consumo, 2)) + ' e vai pagar ' + str(int(casa.valorCompra, 2)) + ' por esse consumo.')
 
-print (line)
+#print (line)
 
-matrix = createMatrix(line)
-for i in range(len(matrix)):
-  print(matrix[i])
+# matrix = createMatrix(line)
+# for i in range(len(matrix)):
+#   print(matrix[i])
